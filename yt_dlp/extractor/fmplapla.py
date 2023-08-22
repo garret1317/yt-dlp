bcf8142eb1fb00b14e27d7cde3071f2932c281a8
garret <garret@airmail.cc>
2023-08-23 00:44:09 +0100

    split into IE and BaseIE in preparation for new site

--- yt_dlp/extractor/fmplapla.py
+++ yt_dlp/extractor/fmplapla.py
@@ -5,20 +5,10 @@ from .common import (
 from ..utils import traverse_obj
 
 
-class FMPlaPlaIE(InfoExtractor):
-    _VALID_URL = r'https://fmplapla\.com/(?P<id>[^/]+)'
-    _TESTS = [{
-        'url': 'https://fmplapla.com/fmnishitokyo',
-        'info_dict': {
-            'title': 're:^エフエム西東京 .+$',
-            'id': 'fmnishitokyo',
-            'ext': 'ogg',
-            'thumbnail': 'https://fmplapla.com/fmnishitokyo/img/artwork.png',
-            'live_status': 'is_live',
-            'token': str,
-        },
-        'params': {'skip_download': True},
-    }]
+class FMPlaPlaBaseIE(InfoExtractor):
+
+    _origin = None
+    _api = None
 
     def _real_extract(self, url):
         station_id = self._match_id(url)
@@ -27,17 +17,18 @@ class FMPlaPlaIE(InfoExtractor):
         if not station_info:
             raise ExtractorError("No such station", expected=True, video_id=station_id)
         thumbnails = []
-        for index, size in enumerate(['favicon', 'icon', 'large_icon', 'artwork']):
-            thumbnails.append({
-                'id': size,
-                'preference': index,
-                'url': station_info.get(size)
-            })
+        for index, size in enumerate(['favicon', 'logo_url', 'icon', 'large_icon', 'artwork']):
+            url = station_info.get(size)
+            if url:
+                thumbnails.append({
+                    'id': size,
+                    'preference': index,
+                    'url': station_info.get(size)
+                })
 
-        stream_info = self._download_json(f'https://fmplapla.com/api/select_stream?station={station_id}&burst=5',
-                                          station_id, headers={"Origin": "https://fmplapla.com"},
-                                          data=b'', note='Getting stream token')
-
+        stream_info = self._download_json(self._api.format(station_id=station_id), station_id,
+                                          headers={"Origin": self._origin}, data=b'',
+                                          note='Getting stream token')
         return {
             'protocol': 'fmplapla',
             'url': stream_info.get('location'),
@@ -52,3 +43,22 @@ class FMPlaPlaIE(InfoExtractor):
             }),
             'thumbnails': thumbnails,
         }
+
+
+class FMPlaPlaIE(FMPlaPlaBaseIE):
+    _VALID_URL = r'https://fmplapla\.com/(?P<id>[^/]+)'
+    _TESTS = [{
+        'url': 'https://fmplapla.com/fmnishitokyo',
+        'info_dict': {
+            'title': 're:^エフエム西東京 .+$',
+            'id': 'fmnishitokyo',
+            'ext': 'ogg',
+            'thumbnail': 'https://fmplapla.com/fmnishitokyo/img/artwork.png',
+            'live_status': 'is_live',
+            'token': str,
+        },
+        'params': {'skip_download': True},
+    }]
+
+    _origin = 'https://fmplapla.com'
+    _api = 'https://fmplapla.com/api/select_stream?station={station_id}&burst=5'
